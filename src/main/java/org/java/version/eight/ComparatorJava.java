a4e3520b309370893f2d73b726e65939973f9b34
Rajesh.Chittavarjula <chramarajesh@gmail.com>
2025-04-10 09:32:14 +0530

    Comminting Comparator and Runnable

--- src/main/java/org/java/version/eight/ComparatorJava.java
+++ src/main/java/org/java/version/eight/ComparatorJava.java
@@ -1,4 +1,23 @@
 package org.java.version.eight;
 
+import java.util.Comparator;
+
 public class ComparatorJava {
+
+    public static void main(String[] args) {
+        Comparator<Integer> comparator=new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        int compare = comparator.compare(0, 3);
+        System.out.println(compare);
+
+//Using Lambda
+        Comparator<Integer> comparatorLambda=(Integer number1, Integer number2) -> number1.compareTo(number2);
+        System.out.println(comparatorLambda.compare(3, 3));
+    }
 }
--- src/main/java/org/java/version/eight/RunnableLambdaEx.java
+++ src/main/java/org/java/version/eight/RunnableLambdaEx.java
@@ -1,4 +1,20 @@
 package org.java.version.eight;
 
 public class RunnableLambdaEx {
+    public static void main(String[] args) {
+        Runnable runnable= new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello Runnable");
+            }
+        };
+        new Thread(runnable).start();
+
+        Runnable runnable1= () -> {
+            System.out.println("Hello Runnable using java 8");
+        };
+        new Thread(runnable1).start();
+    }
+
+
 }
